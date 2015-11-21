4edf322cd2961e80bacd73c9a1efb1de4537dc4a
Sarah Rogers <srogersnz@gmail.com>
2015-11-21 17:17:39 +1300

    Delete calendar components

--- client/app/components/habit_view.jsx
+++ client/app/components/habit_view.jsx
@@ -1,19 +1,14 @@
-import moment from 'moment'
 import React, { Component } from 'react'
 
-require('./habit_view.css')
+// require('./habit_view.css')
 
 class HabitView extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      date: moment()
-    }
   }
 
   render() {
-
     return (
       <div className='test'>
         <p>Hello Simon</p>
