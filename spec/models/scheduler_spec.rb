d13549313c526d3356eadc3d5669116c29430262
Flinn Gendall & Michael McGrath <pair+flinnja.michaelmcgrath@devbootcamp.com>
2015-11-23 12:09:50 +1300

    refactor scheduler to be able to run on single entries in the db or the whole table

--- spec/models/scheduler_spec.rb
+++ spec/models/scheduler_spec.rb
@@ -6,8 +6,12 @@ RSpec.describe Scheduler, type: :model do
     @habit_today = FactoryGirl.create(:habit,:today)
     @habit_tomorrow = FactoryGirl.create(:habit,:tomorrow)
   end
-  it "creates tasks relevant to habits scheduled for today" do
+  it "schedule method creates tasks correctly" do
     expect{Scheduler.schedule}.to change{Task.count}.by(1)
+    expect(Task.first.activity).to eq(@habit_today.name)
+  end
+  it "schedule_single method creates tasks correctly" do
+    expect{Scheduler.schedule_single(Habit.first)}.to change {Task.count}.by(1)
     expect(Task.first.activity).to eq(@habit_today.name)
   end
 end
