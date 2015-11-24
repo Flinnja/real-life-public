6a4a195ba3618615fe5c5a78959a1bd5b67a38a0
Michael McGrath <email": null>
2015-11-24 18:18:01 +1300

    removed calendar component

--- client/app/components/habit_view.jsx
+++ client/app/components/habit_view.jsx
@@ -1,4 +1,3 @@
-import {Calendar} from 'react-calendar-component'
 import moment from 'moment'
 import React, { Component } from 'react'
 
@@ -18,12 +17,7 @@ class HabitView extends Component {
     return (
       <div className='test'>
         <p>Hello Simon</p>
-        <Calendar
-          date={this.state.date}
-          onNextMonth={() => this.setState({ date: this.state.date.clone().add(1, 'months') }) }
-          onPrevMonth={() => this.setState({ date: this.state.date.clone().subtract(1, 'months') }) }
-          onPickDate={(date) => console.log(date)}
-         />
+
       </div>
     )
   }
