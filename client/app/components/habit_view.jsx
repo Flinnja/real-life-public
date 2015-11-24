acb26a94b3694e0a56691daea0e1a045fc7e2df4
Michael McGrath <email": null>
2015-11-24 18:37:33 +1300

    Pushing for Jared

--- client/app/components/habit_view.jsx
+++ client/app/components/habit_view.jsx
@@ -1,7 +1,10 @@
 
-import React, { Component } from 'react'
+
 // var TeX = require('../../react-components/js/tex.jsx')
 var PopUp = require('./popUp')
+
+import React from 'react'
+import request from 'superagent'
 
 
 var HabitButton = require('./habit_button')
@@ -9,6 +12,22 @@ var HabitNavHome = require('./habit_nav_home')
 var HabitCanvas = require('./habit_canvas')
 var HabitNavNeeds = require('./habit_nav_needs')
 
+var HabitView = React.createClass({
+
+  componentWillMount: function () {
+    // var HabitButtonStatus = this.props.habits.length > 0 ? 'edit' : 'add'
+    request
+      .get('/habits/7')
+      .end(function (err, res) {
+        if (err) {
+          console.log("componentWillMount error", err)
+        } else {
+          console.log("componentWillMount response: ", res)
+        }
+      })
+
+    // this.setState({ habitButton: HabitButtonStatus})
+  },
 
 
 class HabitView extends Component {
