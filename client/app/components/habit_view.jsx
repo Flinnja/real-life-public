8abdca7671b9bc562a213d9517cddb3f0b79f8db
Sarah Rogers & Sulu Fiti <pair+sarah-arrrgh.sulufiti@devbootcamp.com>
2015-11-24 15:15:39 +1300

    split out state into top-level "smart component" / container

--- client/app/components/habit_view.jsx
+++ client/app/components/habit_view.jsx
@@ -7,60 +7,23 @@ var HabitNavHome = require('./habit_nav_home')
 var HabitNavNeeds = require('./habit_nav_needs')
 
 var HabitView = React.createClass({
-
-  componentWillMount: function () {
-    // var HabitButtonStatus = this.props.habits.length > 0 ? 'edit' : 'add'
-    request
-      .get('/habits')
-      .set('Accept', 'application/json')
-      .set('Content-Type', 'application/json')
-      .end(function (err, res) {
-        if (err) {
-          console.log("componentWillMount error", err)
-        } else {
-          var habits = JSON.parse(res.text)
-          console.log("componentWillMount response: ", habits.length)
-        }
-      })
-
-    // this.setState({ habitButton: HabitButtonStatus})
-  },
-
-  getInitialState: function () {
-    return {
-      name: 'Simon',
-      showImg: false,
-      habitButton: 'add'
-    }
-  },
-
   render: function () {
     var img = (<img src={this.props.image} />)
 
     console.log('props in HabitView', this.props)
 
     return(
-      <div onClick={this.clickHandler}>
-        {this.state.name}
-        <HabitButton status={this.state.habitButton} onAddHabit={this.onAddHabit.bind(this)} />
+      <div onClick={this.props.clickHandler}>
+        {this.props.name}
+        <HabitButton status={this.props.habitButton} onAddHabit={this.props.onAddHabit} />
         <HabitCanvas />
         <HabitNavHome />
         <HabitNavNeeds />
         <div>
-          { this.state.showImg ? img : null}
+          { this.props.showImg ? img : null}
         </div>
       </div>
     )
-  },
-
-  clickHandler: function () {
-    this.setState({ showImg: !this.state.showImg })
-  },
-
-  onAddHabit: function (status) {
-    console.log('status in HabitView', status)
-
-    this.setState({ habitButton: status })
   }
 })
 
